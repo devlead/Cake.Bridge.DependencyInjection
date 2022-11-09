0d054e040e434a1a7784696f356e8efec361d711
Mattias Karlsson <matkar@wcom.se>
2022-11-10 00:38:17 +0100

    Filescoped namespaces & implicit usings

--- src/Cake.Bridge.DependencyInjection/BridgeScriptHost.cs
+++ src/Cake.Bridge.DependencyInjection/BridgeScriptHost.cs
@@ -1,40 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cake.Core;
-using Cake.Core.Scripting;
-using LogLevel = Cake.Core.Diagnostics.LogLevel;
-using Verbosity = Cake.Core.Diagnostics.Verbosity;
+﻿using Cake.Core.Scripting;
 
-namespace Cake.Bridge.DependencyInjection
+namespace Cake.Bridge.DependencyInjection;
+
+internal class BridgeScriptHost : ScriptHost
 {
-    internal class BridgeScriptHost : ScriptHost
+    private IExecutionStrategy Strategy { get; }
+    private ICakeReportPrinter Reporter { get; }
+
+    public override async Task<CakeReport> RunTargetAsync(string target)
     {
-        private IExecutionStrategy Strategy { get; }
-        private ICakeReportPrinter Reporter { get; }
+        Settings.SetTarget(target);
+        var report = await Engine.RunTargetAsync(Context, Strategy, Settings);
+        Reporter.Write(report);
+        return report;
+    }
 
-        public override async Task<CakeReport> RunTargetAsync(string target)
-        {
-            Settings.SetTarget(target);
-            var report = await Engine.RunTargetAsync(Context, Strategy, Settings);
-            Reporter.Write(report);
-            return report;
-        }
+    public override async Task<CakeReport> RunTargetsAsync(IEnumerable<string> targets)
+    {
+        Settings.SetTargets(targets);
+        var report = await Engine.RunTargetAsync(Context, Strategy, Settings);
+        Reporter.Write(report);
+        return report;
+    }
 
-        public override async Task<CakeReport> RunTargetsAsync(IEnumerable<string> targets)
-        {
-            Settings.SetTargets(targets);
-            var report = await Engine.RunTargetAsync(Context, Strategy, Settings);
-            Reporter.Write(report);
-            return report;
-        }
-
-        public BridgeScriptHost(ICakeEngine engine, ICakeContext context, IExecutionStrategy strategy, ICakeReportPrinter reporter, ICakeArguments arguments)
-            : base(engine, context)
-        {
-            Strategy = strategy;
-            Reporter = reporter;
-        }
+    public BridgeScriptHost(ICakeEngine engine, ICakeContext context, IExecutionStrategy strategy, ICakeReportPrinter reporter, ICakeArguments arguments)
+        : base(engine, context)
+    {
+        Strategy = strategy;
+        Reporter = reporter;
     }
 }
--- src/Cake.Bridge.DependencyInjection/VerbosityConverter.cs
+++ src/Cake.Bridge.DependencyInjection/VerbosityConverter.cs
@@ -1,45 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Globalization;
-using Cake.Core;
-using Cake.Core.Diagnostics;
 
-namespace Cake.Bridge.DependencyInjection
+namespace Cake.Bridge.DependencyInjection;
+
+public sealed class VerbosityConverter : TypeConverter
 {
-    public sealed class VerbosityConverter : TypeConverter
+    private static readonly Dictionary<string, Verbosity> Lookup = new Dictionary<string, Verbosity>(StringComparer.OrdinalIgnoreCase)
     {
-        private static readonly Dictionary<string, Verbosity> Lookup = new Dictionary<string, Verbosity>(StringComparer.OrdinalIgnoreCase)
+        { "q", Verbosity.Quiet },
+        { "quiet", Verbosity.Quiet },
+        { "m", Verbosity.Minimal },
+        { "minimal", Verbosity.Minimal },
+        { "n", Verbosity.Normal },
+        { "normal", Verbosity.Normal },
+        { "v", Verbosity.Verbose },
+        { "verbose", Verbosity.Verbose },
+        { "d", Verbosity.Diagnostic },
+        { "diagnostic", Verbosity.Diagnostic }
+    };
+
+
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    {
+        if (!(value is string stringValue))
         {
-            { "q", Verbosity.Quiet },
-            { "quiet", Verbosity.Quiet },
-            { "m", Verbosity.Minimal },
-            { "minimal", Verbosity.Minimal },
-            { "n", Verbosity.Normal },
-            { "normal", Verbosity.Normal },
-            { "v", Verbosity.Verbose },
-            { "verbose", Verbosity.Verbose },
-            { "d", Verbosity.Diagnostic },
-            { "diagnostic", Verbosity.Diagnostic }
-        };
-
-
-        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-        {
-            if (!(value is string stringValue))
-            {
-                throw new NotSupportedException("Can't convert value to verbosity.");
-            }
-
-            var result = Lookup.TryGetValue(stringValue, out var verbosity);
-            if (result)
-            {
-                return verbosity;
-            }
-
-            const string format = "The value '{0}' is not a valid verbosity.";
-            var message = string.Format(CultureInfo.InvariantCulture, format, value);
-            throw new CakeException(message);
+            throw new NotSupportedException("Can't convert value to verbosity.");
         }
+
+        var result = Lookup.TryGetValue(stringValue, out var verbosity);
+        if (result)
+        {
+            return verbosity;
+        }
+
+        const string format = "The value '{0}' is not a valid verbosity.";
+        var message = string.Format(CultureInfo.InvariantCulture, format, value);
+        throw new CakeException(message);
     }
 }

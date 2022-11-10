ff07039dd8544d0d659def1227b964f46fc19b2a
Mattias Karlsson <matkar@wcom.se>
2022-11-10 20:32:14 +0100

    Filescoped namespace CakeContextCommand

--- src/Cake.Bridge.DependencyInjection.Example/Commands/CakeContextCommand.cs
+++ src/Cake.Bridge.DependencyInjection.Example/Commands/CakeContextCommand.cs
@@ -4,39 +4,38 @@ using Cake.Bridge.DependencyInjection.Example.Commands.Settings;
 using Cake.Common.Diagnostics;
 using Spectre.Console.Cli;
 
-namespace Cake.Bridge.DependencyInjection.Example.Commands
+namespace Cake.Bridge.DependencyInjection.Example.Commands;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class CakeContextCommand : Command<CakeContextSettings>
 {
-    // ReSharper disable once ClassNeverInstantiated.Global
-    public class CakeContextCommand : Command<CakeContextSettings>
+    public override int Execute(CommandContext context, CakeContextSettings settings)
     {
-        public override int Execute(CommandContext context, CakeContextSettings settings)
+        var absoluteSourcePath = settings.Context.MakeAbsolute(settings.SourcePath);
+        var directory = settings.Context.FileSystem.GetDirectory(absoluteSourcePath);
+
+        settings.Context.Information(
+            " Directory of {0}",
+            absoluteSourcePath
+            );
+
+        foreach (var directoryPath in directory.GetDirectories("*.*", SearchScope.Current))
         {
-            var absoluteSourcePath = settings.Context.MakeAbsolute(settings.SourcePath);
-            var directory = settings.Context.FileSystem.GetDirectory(absoluteSourcePath);
-
             settings.Context.Information(
-                " Directory of {0}",
-                absoluteSourcePath
+                "\t<DIR>\t{0}",
+                directoryPath.Path.GetDirectoryName()
                 );
-
-            foreach (var directoryPath in directory.GetDirectories("*.*", SearchScope.Current))
-            {
-                settings.Context.Information(
-                    "\t<DIR>\t{0}",
-                    directoryPath.Path.GetDirectoryName()
-                    );
-            }
-
-            foreach (var file in directory.GetFiles("*.*", SearchScope.Current))
-            {
-                settings.Context.Information(
-                    "\t{0}\t{1}",
-                    file.Length,
-                    file.Path.GetFilename()
-                    );
-            }
-
-            return 0;
         }
+
+        foreach (var file in directory.GetFiles("*.*", SearchScope.Current))
+        {
+            settings.Context.Information(
+                "\t{0}\t{1}",
+                file.Length,
+                file.Path.GetFilename()
+                );
+        }
+
+        return 0;
     }
 }
